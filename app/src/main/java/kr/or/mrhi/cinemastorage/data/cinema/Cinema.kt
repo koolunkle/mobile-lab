a112e0f1e3cf47b0cfb1fce544e3819b29b320c3
koolunkle <a43361904@gmail.com>
2022-11-30 14:21:45 +0900

    [update] data class 추가 및 변경  - backdrop  - release date  - vote average

--- app/src/main/java/kr/or/mrhi/cinemastorage/data/cinema/Cinema.kt
+++ app/src/main/java/kr/or/mrhi/cinemastorage/data/cinema/Cinema.kt
@@ -5,9 +5,15 @@ import com.google.gson.annotations.SerializedName
 data class Cinema(
     @SerializedName("id") val id: Long,
 
+    @SerializedName("backdrop_path") val backdrop: String,
+
+    @SerializedName("poster_path") val poster: String,
+
     @SerializedName("title") val title: String,
 
-    @SerializedName("overview") val overview: String,
+    @SerializedName("release_date") val release: String,
 
-    @SerializedName("poster_path") val posterPath: String
+    @SerializedName("vote_average") val rating: Float,
+
+    @SerializedName("overview") val overview: String
 )
