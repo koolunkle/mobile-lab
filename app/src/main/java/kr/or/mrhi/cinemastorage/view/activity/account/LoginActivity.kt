531e1bce2a6004c6b91078717a5a614a0cb99a10
koolunkle <a43361904@gmail.com>
2022-12-01 17:48:33 +0900

    [create] Login Activity 추가  - check duplicate value

--- app/src/main/java/kr/or/mrhi/cinemastorage/view/activity/account/LoginActivity.kt
+++ app/src/main/java/kr/or/mrhi/cinemastorage/view/activity/account/LoginActivity.kt
@@ -2,7 +2,13 @@ package kr.or.mrhi.cinemastorage.view.activity.account
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import kr.or.mrhi.cinemastorage.dao.UserDAO
+import kr.or.mrhi.cinemastorage.data.User
 import kr.or.mrhi.cinemastorage.databinding.ActivityLoginBinding
 import kr.or.mrhi.cinemastorage.view.activity.MainActivity
 
@@ -11,6 +17,8 @@ class LoginActivity : AppCompatActivity() {
     private var _binding: ActivityLoginBinding? = null
 
     private val binding get() = _binding!!
+
+    private var isUser = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +32,38 @@ class LoginActivity : AppCompatActivity() {
 
     private fun moveToMain() {
         binding.btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val nickname = binding.edtNickname.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            if (binding.edtNickname.text.isEmpty() || binding.edtPassword.text.isEmpty()) {
+                setToast("Please enter your nickname and password")
+                return@setOnClickListener
+            }
+
+            val userDAO = UserDAO()
+            val intent = Intent(applicationContext, MainActivity::class.java)
+
+            userDAO.databaseReference?.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (data in snapshot.children) {
+                        val user = data.getValue(User::class.java)
+                        if (nickname == user?.nickname && password == user.password) isUser = true
+                    }
+                    if (isUser) {
+                        setToast("Login Successful")
+                        startActivity(intent)
+                    } else setToast("Nickname or Password does not match")
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    setToast(error.message)
+                }
+            })
         }
+    }
+
+    private fun setToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
