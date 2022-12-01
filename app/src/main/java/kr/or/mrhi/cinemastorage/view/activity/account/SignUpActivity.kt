56fdfc3d5016f98c1000c05c69834581f2dd07bd
koolunkle <a43361904@gmail.com>
2022-12-01 15:12:57 +0900

    [create] SignUp Activity 추가  - check duplicate value

--- app/src/main/java/kr/or/mrhi/cinemastorage/view/activity/account/SignUpActivity.kt
+++ app/src/main/java/kr/or/mrhi/cinemastorage/view/activity/account/SignUpActivity.kt
@@ -5,17 +5,17 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import kr.or.mrhi.cinemastorage.dao.UserDAO
 import kr.or.mrhi.cinemastorage.data.User
 import kr.or.mrhi.cinemastorage.databinding.ActivitySignUpBinding
+import java.io.File
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -23,74 +23,97 @@ class SignUpActivity : AppCompatActivity() {
 
     private val binding get() = _binding!!
 
-    private var imageUri: Uri? = null
-
-    lateinit var filePath: String
-
-    lateinit var database: DatabaseReference
+    private lateinit var filePath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        database = Firebase.database.reference
+        binding.apply {
+            getExternalContentUri()
+            uploadExternalContentUri()
+        }
+    }
 
-        var requestLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()) {
+    private fun uploadExternalContentUri() {
+        binding.btnSignUp.setOnClickListener {
+            val nickname = binding.edtNickname.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            val userDAO = UserDAO()
+            val userKey = userDAO.databaseReference?.push()?.key
+            val user = User(userKey.toString(), nickname, password)
+
+            val imageReference =
+                userDAO.storage?.reference?.child("images/${userKey}.jpg")
+            val file = Uri.fromFile(File(filePath))
+
+            if (binding.ivProfile.drawable == null || binding.edtNickname.text.isEmpty() || binding.edtPassword.text.isEmpty()) {
+                setToast("Please enter your profile picture, nickname, and password at all")
+                return@setOnClickListener
+            } else {
+                userDAO.databaseReference?.addListenerForSingleValueEvent(object :
+                    ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val child: Iterator<DataSnapshot> = snapshot.children.iterator()
+                        while (child.hasNext()) {
+                            if (nickname == child.next().key) {
+                                setToast("Duplicate nickname")
+                                userDAO.databaseReference!!.removeEventListener(this)
+                                return
+                            }
+                        }
+                        imageReference?.putFile(file)?.apply {
+                            addOnSuccessListener {
+                                userDAO.databaseReference?.child(nickname)?.setValue(user)
+                                    .apply {
+                                        addOnSuccessListener { setToast("Success to insert data") }
+                                        addOnFailureListener { setToast("Failed to insert data") }
+                                    }
+                            }
+                            addOnFailureListener { setToast("Failed to insert data") }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        setToast(error.message)
+                    }
+                })
+            }
+        }
+    }
+
+    private fun getExternalContentUri() {
+        val requestLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
             if (it.resultCode == Activity.RESULT_OK) {
-                Glide.with(applicationContext)
-                    .load(it.data?.data)
-                    .into(binding.ivProfile)
-                imageUri = it.data?.data
-                var cursor = contentResolver.query(it.data?.data as Uri,
-                    arrayOf<String>(MediaStore.Images.Media.DATA), null, null, null)
+                Glide.with(this).load(it.data?.data).into(binding.ivProfile)
+
+                val cursor = contentResolver.query(
+                    it.data?.data!!, arrayOf(MediaStore.Images.Media.DATA), null, null, null
+                )
                 cursor?.moveToFirst().let {
-                    filePath = cursor!!.getString(0) /*절대경로*/
+                    if (cursor != null) filePath = cursor.getString(0)
+                    cursor?.close()
                 }
             }
         }
-
-        /*갤러리에서 이미지파일 골라와서 프로필 이미지에 넣기*/
         binding.ivProfile.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
             intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
             requestLauncher.launch(intent)
         }
+    }
 
-        /*이미지, 닉네임, 비밀번호 입력 후 signUp 버튼 눌렀을때 firebase에 데이터 보냄*/
-        binding.btnSignUp.setOnClickListener {
-            if (binding.ivProfile.drawable == null || binding.edtNickname.text.isEmpty() || binding.edtPassword.text.isEmpty()) {
-                Toast.makeText(this,
-                    "Please enter your profile picture, nickname, and password at all.",
-                    Toast.LENGTH_SHORT).show()
-            } else {
-                val nickname = binding.edtNickname.text.toString()
-                val pw = binding.edtPassword.text.toString()
-
-                FirebaseStorage.getInstance()
-                    .reference.child("userProfileImage").child("$nickname/photo")
-                    .putFile(imageUri!!).addOnSuccessListener {
-                        var userProfile: Uri? = null
-                        FirebaseStorage.getInstance().reference.child("userProfileImage")
-                            .child("$nickname/photo").downloadUrl.addOnSuccessListener {
-                                userProfile = it
-                                Log.d("SignUpActivity", " successful user upload")
-                                val user = User("",
-                                    nickname.toString(),
-                                    pw.toString(),
-                                    userProfile.toString())
-                                database.child("users").child(nickname.toString()).setValue(user)
-                            }
-                    }
-                Toast.makeText(this, "회원가입완료", Toast.LENGTH_SHORT).show()
-                startActivity(intent)
-            }
-        }
+    private fun setToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         _binding = null
         super.onDestroy()
     }
+
 }
