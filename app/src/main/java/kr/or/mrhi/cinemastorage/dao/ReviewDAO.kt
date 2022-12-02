1ef625daafe90317c4c20984b61d357fc6b548e1
koolunkle <a43361904@gmail.com>
2022-12-02 17:22:14 +0900

    [create] ReviewDAO 추가  - storage

--- app/src/main/java/kr/or/mrhi/cinemastorage/dao/ReviewDAO.kt
+++ app/src/main/java/kr/or/mrhi/cinemastorage/dao/ReviewDAO.kt
@@ -4,16 +4,23 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.Query
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import kr.or.mrhi.cinemastorage.data.Review
 
 class ReviewDAO {
     /*firebaseRealtimeDatabase reviewTbl */
     var databaseReference: DatabaseReference? = null
 
+    /*firebaseStorage*/
+    var storage: FirebaseStorage? = null
+
     init {
         /*get instance reviewTbl @realtimeDB */
         val db: FirebaseDatabase = FirebaseDatabase.getInstance()
         databaseReference = db.getReference("review")
+        storage = Firebase.storage
     }
 
     /*insert into reviewTbl @realtimeDB*/
