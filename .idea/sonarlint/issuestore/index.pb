fbd7d662886cebbb309f6121752203418b431275
eunkeem <quercus7868@gmail.com>
2022-12-05 11:20:01 +0900

    [update] build.gradle 수정 -property에 key넣어두고 이름으로 불러서 사용하도록 수정

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,5 +6,15 @@ Happ/src/main/java/kr/or/mrhi/cinemastorage/view/fragment/ListFragment.kt,d\7\d
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-@
-app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+
+Qapp/src/main/java/kr/or/mrhi/cinemastorage/view/activity/user/PersonalActivity.kt,c\f\cfdf5bfa037829a19d89a5a24cddc4ace9bc11cc
+‡
+Wapp/src/main/java/kr/or/mrhi/cinemastorage/view/activity/user/UpdateUserinfoActivity.kt,0\a\0a313e2a78cd43337b0e40e49df4d6aac9b776f1
+g
+7app/src/main/java/kr/or/mrhi/cinemastorage/data/User.kt,f\3\f3f9bb7d2cd3140fcd0f2f1115609f508d15a3f7
+
+Qapp/src/main/java/kr/or/mrhi/cinemastorage/view/activity/account/LoginActivity.kt,0\6\06e13a8d886c4e9300cadf2f31b52359b42f3d2f
+x
+Happ/src/main/java/kr/or/mrhi/cinemastorage/view/activity/MainActivity.kt,0\1\01f61c21e7899a6428ccd768276ed57918e1c373
+€
+Papp/src/main/java/kr/or/mrhi/cinemastorage/view/activity/ReviewDetailActivity.kt,f\6\f6c82137c13e15ba8d66d7d414d46fa83ec6698a
--- app/src/main/java/kr/or/mrhi/cinemastorage/network/CinemaAPI.kt
+++ app/src/main/java/kr/or/mrhi/cinemastorage/network/CinemaAPI.kt
@@ -1,5 +1,6 @@
 package kr.or.mrhi.cinemastorage.network
 
+import androidx.viewbinding.BuildConfig
 import kr.or.mrhi.cinemastorage.data.cinema.GetCinemaResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,19 +10,19 @@ interface CinemaAPI {
 
     @GET("movie/popular")
     fun getPopularCinema(
-        @Query("api_key") apiKey: String = "1a5e97ab7807cdecbc973e10e2ecf2ae",
+        @Query("api_key") apiKey: String = "${kr.or.mrhi.cinemastorage.BuildConfig.POPULAR_KEY}",
         @Query("page") page: Int
     ): Call<GetCinemaResponse>
 
     @GET("movie/top_rated")
     fun getTopRatedCinema(
-        @Query("api_key") apiKey: String = "1a5e97ab7807cdecbc973e10e2ecf2ae",
+        @Query("api_key") apiKey: String = "${kr.or.mrhi.cinemastorage.BuildConfig.TOP_RATE_KEY}",
         @Query("page") page: Int
     ): Call<GetCinemaResponse>
 
     @GET("movie/upcoming")
     fun getUpcomingCinema(
-        @Query("api_key") apiKey: String = "1a5e97ab7807cdecbc973e10e2ecf2ae",
+        @Query("api_key") apiKey: String = "${kr.or.mrhi.cinemastorage.BuildConfig.UPCOMING_KEY}",
         @Query("page") page: Int
     ): Call<GetCinemaResponse>
 }
