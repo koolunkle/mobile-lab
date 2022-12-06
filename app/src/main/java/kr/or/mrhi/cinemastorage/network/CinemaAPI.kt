7ef8e09b2d7fabb017c74cb8c18a79c982a9eda8
koolunkle <a43361904@gmail.com>
2022-12-06 09:13:45 +0900

    [update] etc.  - renamed  - remove duplicate code

--- app/src/main/java/kr/or/mrhi/cinemastorage/network/CinemaAPI.kt
+++ app/src/main/java/kr/or/mrhi/cinemastorage/network/CinemaAPI.kt
@@ -1,5 +1,6 @@
 package kr.or.mrhi.cinemastorage.network
 
+import kr.or.mrhi.cinemastorage.BuildConfig
 import kr.or.mrhi.cinemastorage.data.cinema.GetCinemaResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,19 +10,19 @@ interface CinemaAPI {
 
     @GET("movie/popular")
     fun getPopularCinema(
-        @Query("api_key") apiKey: String = "${kr.or.mrhi.cinemastorage.BuildConfig.POPULAR_KEY}",
+        @Query("api_key") apiKey: String = BuildConfig.POPULAR_KEY,
         @Query("page") page: Int,
     ): Call<GetCinemaResponse>
 
     @GET("movie/top_rated")
     fun getTopRatedCinema(
-        @Query("api_key") apiKey: String = "${kr.or.mrhi.cinemastorage.BuildConfig.TOP_RATE_KEY}",
+        @Query("api_key") apiKey: String = BuildConfig.TOP_RATE_KEY,
         @Query("page") page: Int,
     ): Call<GetCinemaResponse>
 
     @GET("movie/upcoming")
     fun getUpcomingCinema(
-        @Query("api_key") apiKey: String = "${kr.or.mrhi.cinemastorage.BuildConfig.UPCOMING_KEY}",
+        @Query("api_key") apiKey: String = BuildConfig.UPCOMING_KEY,
         @Query("page") page: Int,
     ): Call<GetCinemaResponse>
 }
